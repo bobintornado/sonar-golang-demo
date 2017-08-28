9e1d49fbb780d5606db4e0266b5eb2bf3a9f6043
Bob Cao <bobintornado@gmail.com>
2017-08-28 19:53:03 +0800

    Add more codes

---
+++ template.go
@@ -0,0 +1,18 @@
+package samples
+
+import (
+	"html/template"
+	"os"
+)
+
+const tmpl = ""
+
+func main() {
+	a := "something from another place"
+	t := template.Must(template.New("ex").Parse(tmpl))
+	v := map[string]interface{}{
+		"Title": "Test <b>World</b>",
+		"Body":  template.JS(a),
+	}
+	t.Execute(os.Stdout, v)
+}
